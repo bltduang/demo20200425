09a6948f95a2414abec88ccf091801852677411a
blt2 <blt_duang@163.com>
2020-04-25 17:42:18 +0800

    Spring Boot中使用JdbcTemplate访问数据库

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import com.example.service.UserService;
 import com.example.web.HelloController;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -22,22 +25,34 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 class DemoApplicationTests {
 
-	private MockMvc mvc;
+	@Autowired
+	private UserService userSerivce;
 
 	@Before
-	public void setUp() throws Exception {
-		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+	public void setUp() {
+		// 准备，清空user表
+		userSerivce.deleteAllUsers();
 	}
 
 	@Test
-	public void getHello() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Hello World")));
-	}
+	public void test() throws Exception {
+		// 插入5个用户
+		userSerivce.create("a", 1);
+		userSerivce.create("b", 2);
+		userSerivce.create("c", 3);
+		userSerivce.create("d", 4);
+		userSerivce.create("e", 5);
 
-	@Test
-	void contextLoads() {
+		// 查数据库，应该有5个用户
+		Assert.assertEquals(5, userSerivce.getAllUsers().intValue());
+
+		// 删除两个用户
+		userSerivce.deleteByName("a");
+		userSerivce.deleteByName("e");
+
+		// 查数据库，应该有5个用户
+		Assert.assertEquals(3, userSerivce.getAllUsers().intValue());
+
 	}
 
 }
