ad9e5b736d73a48aefb293caa574bdd4895450d3
blt2 <blt_duang@163.com>
2020-04-26 09:45:53 +0800

    Spring Boot中使用Spring-data-jpa让数据访问更简单、更优雅

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.example.domain.User;
+import com.example.service.UserRepository;
 import com.example.service.UserService;
 import com.example.web.HelloController;
 import org.junit.Assert;
@@ -25,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 class DemoApplicationTests {
 
+	//JDBCTemplate测试
 	@Autowired
 	private UserService userSerivce;
 
@@ -35,7 +38,7 @@ class DemoApplicationTests {
 	}
 
 	@Test
-	public void test() throws Exception {
+	public void test1() throws Exception {
 		// 插入5个用户
 		userSerivce.create("a", 1);
 		userSerivce.create("b", 2);
@@ -52,6 +55,45 @@ class DemoApplicationTests {
 
 		// 查数据库，应该有5个用户
 		Assert.assertEquals(3, userSerivce.getAllUsers().intValue());
+
+	}
+
+	//Spring-data-jpa测试
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	public void test2() throws Exception {
+
+		// 创建10条记录
+		userRepository.save(new User("AAA", 10));
+		userRepository.save(new User("BBB", 20));
+		userRepository.save(new User("CCC", 30));
+		userRepository.save(new User("DDD", 40));
+		userRepository.save(new User("EEE", 50));
+		userRepository.save(new User("FFF", 60));
+		userRepository.save(new User("GGG", 70));
+		userRepository.save(new User("HHH", 80));
+		userRepository.save(new User("III", 90));
+		userRepository.save(new User("JJJ", 100));
+
+		// 测试findAll, 查询所有记录
+		Assert.assertEquals(10, userRepository.findAll().size());
+
+		// 测试findByName, 查询姓名为FFF的User
+		Assert.assertEquals(60, userRepository.findByName("FFF").getAge().longValue());
+
+		// 测试findUser, 查询姓名为FFF的User
+		Assert.assertEquals(60, userRepository.findUser("FFF").getAge().longValue());
+
+		// 测试findByNameAndAge, 查询姓名为FFF并且年龄为60的User
+		Assert.assertEquals("FFF", userRepository.findByNameAndAge("FFF", 60).getName());
+
+		// 测试删除姓名为AAA的User
+		userRepository.delete(userRepository.findByName("AAA"));
+
+		// 测试findAll, 查询所有记录, 验证上面的删除是否成功
+		Assert.assertEquals(9, userRepository.findAll().size());
 
 	}
 
