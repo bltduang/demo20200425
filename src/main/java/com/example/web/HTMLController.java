7330392f8a14cdfefcdadfd14eeada1785691634
blt2 <blt_duang@163.com>
2020-04-29 15:01:32 +0800

    xtjt导航页面localhost/index

--- src/main/java/com/example/web/HTMLController.java
+++ src/main/java/com/example/web/HTMLController.java
@@ -10,11 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HTMLController {
 
-    @RequestMapping("/index")
+    @RequestMapping("/index1")
     public String index1(ModelMap map){
         //加入一个属性，用来在模板中读取
         map.addAttribute("host","http://47.93.36.145:8080/travel");
-        // return模板文件的名称，对应src/main/resources/templates/index.html
+        // return模板文件的名称，对应src/main/resources/templates/index1.html
+        return "index1";
+    }
+    @RequestMapping("/index")
+    public String index(ModelMap map){
+        //加入一个属性，用来在模板中读取
+        map.addAttribute("host","http://47.93.36.145:8080/travel");
+        // return模板文件的名称，对应src/main/resources/templates/index1.html
         return "index";
     }
 }
